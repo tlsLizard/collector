3035ce41f0092c340ec4254c71d2c3304fbfaa9b
Yishan SUN <sandrine.sun.yishan@gmail.com>
2021-01-21 17:48:10 +0100

    Ajouter un bouton quit pour add et delete

--- del_line_gui.py
+++ del_line_gui.py
@@ -18,27 +18,22 @@ class Ui_del_ui(object):#changement de nom
         db_ui.setObjectName("db_ui")
         db_ui.resize(388, 187)
         self.label = QtWidgets.QLabel(db_ui)
-        self.label.setGeometry(QtCore.QRect(100, 30, 201, 21))
+        self.label.setGeometry(QtCore.QRect(70, 30, 261, 21))
         self.label.setObjectName("label")
         self.delete_line = QtWidgets.QPushButton(db_ui)
-        self.delete_line.setGeometry(QtCore.QRect(190, 110, 90, 28))
+        self.delete_line.setGeometry(QtCore.QRect(200, 130, 90, 28))
         self.delete_line.setObjectName("delete_line")
         self.object_id = QtWidgets.QLineEdit(db_ui)
-        self.object_id.setGeometry(QtCore.QRect(90, 80, 61, 28))
+        self.object_id.setGeometry(QtCore.QRect(140, 70, 101, 28))
         self.object_id.setObjectName("object_id")
-        #########################################################
-        self.quit_btn = QtWidgets.QPushButton(del_line_ui)
-        self.quit_btn.setGeometry(QtCore.QRect(90, 130, 90, 28))
-        self.quit_btn.setObjectName("quit_btn")
-        self.quit_btn.clicked.connect(db_ui.close)
         ###########################################################
-        self.quit_btn.clicked.connect(lambda: delete_line(int(self.object_id.text())))
+        self.delete_line.clicked.connect(lambda: delete_line(int(self.object_id.text())))
         ##renvoyer un message d'erreur si l'id n'a pas d'entree dans la table
         ###########################################################
-        self.msg_box = QtWidgets.QLabel(db_ui)
-        self.msg_box.setGeometry(QtCore.QRect(90, 120, 58, 16))
-        self.msg_box.setText("coucou")
-        self.msg_box.setObjectName("msg_box")
+        self.quit_btn = QtWidgets.QPushButton(db_ui)
+        self.quit_btn.setGeometry(QtCore.QRect(80, 130, 90, 28))
+        self.quit_btn.setObjectName("quit_btn")
+        self.quit_btn.clicked.connect(db_ui.close)
 
         self.retranslateUi(db_ui)
         QtCore.QMetaObject.connectSlotsByName(db_ui)
@@ -60,4 +55,3 @@ if __name__ == "__main__":
     ui.setupUi(db_ui)
     db_ui.show()
     sys.exit(app.exec_())
-
